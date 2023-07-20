1370f5dd5216d5fb3cfa121b4c91973a5b9ea34e
Ricardo Miranda <ricardo.a.miranda@outlook.com>
2023-07-20 18:09:51 -0300

    Exercise about Tuples - First commit

---
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+
+        string[] numbers = phoneNumber.Split('-');
+        return (numbers[0] == "212", numbers[1] == "555", numbers[2]);
+
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+    {
+        return phoneNumberInfo.IsFake == true;
+    }
+}
